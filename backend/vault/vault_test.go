1af14afef9f82ccd069b90c79386ae0b8df0c3f8
Martin Czygan <martin.czygan@gmail.com>
2022-07-07 13:47:17 +0200

    vault: allow test instance be passed via env
    Example:
    
      $ VAULT_TEST_REMOTE_NAME=v: go test -v ./backend/vault/...

--- backend/vault/vault_test.go
+++ backend/vault/vault_test.go
@@ -1,16 +1,24 @@
 package vault
 
 import (
+	"os"
 	"testing"
 
 	"github.com/rclone/rclone/fstest/fstests"
 )
 
-// TestIntegration runs integration tests against the remote
+// TestIntegration runs integration tests against the remote. Run:
+//
+//   $ VAULT_TEST_REMOTE_NAME=v: go test -v ./backend/vault/...
+//
 func TestIntegration(t *testing.T) {
 	// TODO: Setup fresh vault, e.g. with testcontainers.
+	remoteName := "VaultTest:"
+	if v := os.Getenv("VAULT_TEST_REMOTE_NAME"); v != "" {
+		remoteName = v
+	}
 	fstests.Run(t, &fstests.Opt{
-		RemoteName: "VaultTest:",
+		RemoteName: remoteName,
 		NilObject:  (*Object)(nil),
 	})
 }
